1db5022701f70a274d895bea0a89775500afe630
kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2024-05-08 11:58:56 +0700

    Test heirachy page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,143 @@
-import Image from "next/image";
+'use client'
+import React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
+import { TreeItem, treeItemClasses } from '@mui/x-tree-view/TreeItem';
+import Image from 'next/image';
 
-export default function Home() {
+const StyledTreeItem = styled(TreeItem)(({ theme }) => ({
+  color:
+    theme.palette.mode === 'light' ? theme.palette.grey[800] : theme.palette.grey[200],
+
+  [`& .${treeItemClasses.content}`]: {
+    borderRadius: theme.spacing(0.5),
+    padding: theme.spacing(0.5, 1),
+    margin: theme.spacing(0.2, 0),
+    [`& .${treeItemClasses.label}`]: {
+      fontSize: '0.8rem',
+      fontWeight: 500,
+    },
+  },
+  [`& .${treeItemClasses.iconContainer}`]: {
+    borderRadius: '50%',
+    backgroundColor:
+      theme.palette.mode === 'light'
+        ? alpha(theme.palette.primary.main, 0.25)
+        : theme.palette.primary.dark,
+    color: theme.palette.mode === 'dark' && theme.palette.primary.contrastText,
+    padding: theme.spacing(0, 1.2),
+  },
+}));
+
+// Mock data JSON representing the tree hierarchy
+const mockData = [
+  {
+    id: '1',
+    label: 'BlS',
+    children: [
+      {
+        id: '2',
+        label: 'Hello',
+      },
+      {
+        id: '3',
+        label: 'Subtree with children',
+        children: [
+          {
+            id: '6',
+            label: 'Hello',
+          },
+          {
+            id: '7',
+            label: 'Sub-subtree with children',
+            children: [
+              {
+                id: '9',
+                label: 'Child 1',
+              },
+              {
+                id: '10',
+                label: 'Child 2',
+              },
+              {
+                id: '11',
+                label: 'Child 3',
+              },
+            ],
+          },
+          {
+            id: '8',
+            label: 'Hello',
+          },
+        ],
+      },
+      {
+        id: '4',
+        label: 'World',
+      },
+      {
+        id: '5',
+        label: 'Something something',
+      },
+    ],
+  },
+  {
+    id: '3000',
+    label: 'BCAP'
+  }
+];
+
+// Function to recursively sort the tree data by label
+const sortTree = (data:any) => {
+  if (!data) return null;
+  data.sort((a: any, b: any) => a.label.localeCompare(b.label));
+  data.forEach((node: any) => sortTree(node.children));
+};
+
+const Home = () => {
+  const sortedData = [...mockData];
+  sortTree(sortedData);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen justify-between bg-white">
+      <section id="header">
+        <div className="bg-rose-500 p-2"></div>
+      </section>
+
+      <section id="logo">
+        <div className="flex content-center">
+          <div>
+            <Image src="/bls-header-logo.png" alt="" width={300} height={300}></Image>
+          </div>
+          <div>
+            <p className="p-5 font-bold text-3xl text-rose-600">StaffInformation</p>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <hr></hr>
+      {/* side bar Department */}
+      <section id='sidebar'>
+        <div className='p-5'>
+          <SimpleTreeView
+            aria-label="customized"
+            defaultExpandedItems={['1']}
+            sx={{ overflowX: 'hidden', minHeight: 270, flexGrow: 1, maxWidth: 300 }}
+          >
+            <StyledTreeItem itemId="0" label="Home"></StyledTreeItem>
+            <StyledTreeItem itemId="20" label="Search Information"></StyledTreeItem>
+            {sortedData.map((node) => (
+              <StyledTreeItem key={node.id} itemId={node.id} label={node.label}>
+                {node.children && node.children.map((child) => (
+                  <StyledTreeItem key={child.id} itemId={child.id} label={child.label} />
+                ))}
+              </StyledTreeItem>
+            ))}
+          </SimpleTreeView>
+        </div>
+      </section>
     </main>
   );
-}
+};
+
+export default Home;
