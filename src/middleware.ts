1ee93bab08fbe35d6bc1b655884c3f111481a3fa
kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2025-07-23 12:25:15 +0700

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,26 @@
 // middleware.ts
-// import { NextRequest, NextResponse } from 'next/server'
-// import { cookies } from 'next/headers'
-// import { parseJwt } from './app/utils/auth';
+import { NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import { parseJwt } from './app/utils/auth';
 
-// export async function middleware(request: NextRequest) {
-//   const cookieStore = cookies();
-//   const sessionCookie = cookieStore.get('auth_token');
-//   const session: string | undefined = sessionCookie?.value;
+export async function middleware(request: NextRequest) {
+  const cookieStore = cookies();
+  const sessionCookie = cookieStore.get('auth_token');
+  const session: string | undefined = sessionCookie?.value;
 
-//   if (!session) {
-//     return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_AUTH_URL}/login`, request.url));
-//   }
+  if (!session) {
+    return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_AUTH_URL}/login`, request.url));
+  }
 
-//   const decodedToken: any = await parseJwt(session);
+  // const decodedToken: any = await parseJwt(session);
 
-//   if (!decodedToken || !decodedToken.roles || decodedToken.roles.length === 0) {
-//     return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_AUTH_URL}/login`, request.url));
-//   }
+  // if (!decodedToken || !decodedToken.roles || decodedToken.roles.length === 0) {
+  //   return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_AUTH_URL}/login`, request.url));
+  // }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ['/:manage/Management'],
-// }
+export const config = {
+  matcher: ['/:manage/Management'],
+}
